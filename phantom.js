ab11173813de817bb0c91cb0df1fe48fa5e9f9ea
Albert <sero.albert@gmail.com>
2016-01-27 19:42:10 -0800

    Add comment

--- phantom.js
+++ phantom.js
@@ -1,9 +1,12 @@
 var webpage = require('webpage');
 var page = webpage.create();
 
+// Global loading flog
 var loading = false;
+// Global load jquery flag
 var loadJQ = false;
 
+// Credentials file
 var config = require('./config');
 
 page.onLoadStarted = function() {
@@ -15,6 +18,7 @@ page.onLoadFinished = function() {
   loading = false;
 };
 
+// Step index
 var index = 0;
 
 var data = {};
@@ -24,68 +28,84 @@ var steps = [
     page.open("https://www.att.com");
   },
   function() {
+    // Enter credentials
     page.evaluate(function(user, password){
       $('form#ssoLoginForm input#userid').val(user);
       $('form#ssoLoginForm input#userPassword').val(password);
     }, config.user, config.password);
   },
   function() {
+    // Enter login
     page.evaluate(function() {
       $('form#ssoLoginForm').submit();
     });
   },
   function() {
+    // Hack to wait for passcode field to appear
     var result = page.evaluate(function() {
       return document.querySelector("form input#passcode0");
     });
     if (!result) {
       index--;
     } else {
+      // Enter passcode
       page.evaluate(function(passcode) {
         $('form input#passcode0').val(passcode);
       }, config.passcode);
     }
   },
   function() {
+    // Login
     page.evaluate(function() {
       $('.btnRt .button.small.primary').click()
     })
   },
   function() {
+    // Go to bill detail page
     page.open("https://www.att.com/olam/passthroughAction.myworld?actionType=ViewBillDetails");
   },
   function() {
+    // Hack to wait for report
     var result = page.evaluate(function() {
       return $(".bPOD-bill-title.PadBot3imp.MarTop10:nth(1)");
     });
     if (!result) {
       index--;
     } else {
+      // Following code is compiled from coffeescript, may be confusing
       data.lines = page.evaluate(function() {
         var SUBTITLE_REGEX, bill, number, numberTitle, ref, ref1, ref2, subGroup;
         bill = {};
+        // Regex for matching item title
         SUBTITLE_REGEX = /(Monthly|Surcharges|Government|Data|Equipment|International|Smartphone|iPhone|30GB|National|Access)/;
+        // Div containing the line number
         numberTitle = $('.bPOD-bill-title.PadBot3imp.MarTop10:nth(1)').next();
+        // Srapge until it cannot find the line number div
         while (numberTitle) {
+          // Scrape the line number
           number = (ref = numberTitle.find('a')) != null ? (ref1 = ref.text()) != null ? (ref2 = ref1.match(/(WENGCHEONG SIN )([0-9\-]+)/)) != null ? ref2[2] : void 0 : void 0 : void 0;
           if (!number) {
             break;
           }
+          // Bill table for the line
           subGroup = numberTitle.next();
           bill[number] = {};
           subGroup.find('.sub-group-title').each(function(index, elem) {
             var ref3, title;
             title = (ref3 = $(elem).find('a').text().match(SUBTITLE_REGEX)) != null ? ref3[1].toLowerCase() : void 0;
             if (title === 'monthly') {
+              // Go into details of monthly charges
               return $(elem).next().find('.botDotBorder, .BotSolidBorder').each(function(index, elem) {
                 var ref4;
                 title = (ref4 = $(elem).find('.accRow:nth(0)').text().match(SUBTITLE_REGEX)) != null ? ref4[1].toLowerCase() : void 0;
                 return bill[number][title] = parseFloat($(elem).find('.accRow:nth(1)').text().replace(/( |\$|\n|\t)/g, '').replace(/−/g, '-'));
               });
             } else {
+              // Otherwise just save the amount with title
               return bill[number][title] = parseFloat($(elem).find('.flipper').text().replace(/( |\$|\n|\t)/g, '').replace(/−/g, '-'));
             }
           });
+          // Scrape next line
           numberTitle = subGroup.next();
         }
         return bill;
@@ -93,19 +113,21 @@ var steps = [
     }
   },
   function() {
+    // Go to usage detail page
     page.open("https://www.att.com/olam/billUsageTiles.myworld");
   },
   function() {
+    // Hack again
     var result = page.evaluate(function() {
       return $('#usageTableToggleId tbody tr');
     });
     if (!result) {
       index--;
     } else {
+      // Scrape usages
       data.usages = page.evaluate(function() {
         var usages;
         usages = {};
-
         $('#usageTableToggleId tbody tr').each(function(index, elem) {
           var number, usage;
           number = $(elem).find('td:nth(0) span').text().replace(/(\n| |\t)/g, '').replace(/\./g, '-');
@@ -119,17 +141,22 @@ var steps = [
 ]
 
 
+// Steps iterator with 2s interval
 setInterval(function() {
+  // Only proceed to next step when page is fully loaded, otherwise, wait for next iteration
   if (!loading && steps[index]) {
     if (loadJQ) {
+      // Inject jquery for scraping if it hasn't been injected yet
       page.injectJs("jquery-1.12.0.min.js")
       loadJQ = false;
     }
     steps[index]();
     index++;
   }
+  // Finshed
   if (!steps[index]) {
     var fs = require('fs');
+    // Write data to data.json
     fs.write('data.json', JSON.stringify(data, null, 2), 'w');
     phantom.exit();
   }
--- server.coffee
+++ server.coffee
@@ -16,6 +16,7 @@ app.set 'view engine', 'ejs'
 app.use bodyParser.json
   limit: 104900000
 
+# Prettify item title
 app.locals.TITLES =
   'smartphone': 'Line Base Charge'
   'iphone': 'Line Base Charge'
@@ -38,6 +39,7 @@ app.get '/', (req, res, next) ->
     content = fs.readFileSync './data.json', 'utf8'
     data = JSON.parse(content)
   catch e
+    # Render loading page if data.json not exist
     return res.render 'loading'
 
   bill =
@@ -47,44 +49,56 @@ app.get '/', (req, res, next) ->
     lineCount: 0
     lines: {}
     overage: {amount: 0, total: 0, lines: {}}
+
   for number, detail of data.lines
     bill.lineCount += 1
     bill.lines[number] = {}
     for title, amount of detail
       if title in ['30gb', 'national']
+        # Add to plan charges
         bill.plan[title] ||= 0
         bill.plan[title] += amount
       else if title in ['data']
+        # Add to overage chrages
         bill.overage.amount += amount
       else if title in ['surcharges', 'government']
+        # Add to surcharges
         bill.surcharges[title] ||= 0
         bill.surcharges[title] += amount
       else
+        # Line's charges
         bill.lines[number][title] = amount
 
+  # Compute total of surcharges and plan charges
   bill.surcharges.total = _.sum _.values(bill.surcharges)
 
   bill.plan.total = _.sum _.values(bill.plan)
 
+  # Compute overage usages
   for number, usage of data.usages
-    if usage > (30 / bill.lineCount)
+    if usage > (30 / bill.lineCount) # Over average usage
       bill.overage.total += usage - (30 / bill.lineCount)
       bill.overage.lines[number] = usage - (30 / bill.lineCount)
 
   total = 0
+  # Distribute overage charges
   for number, line of bill.lines
     line.shared = bill.plan.total / bill.lineCount
     if bill.overage.lines[number]
       line.overage = bill.overage.amount * (bill.overage.lines[number] / bill.overage.total)
+    # Calculate tmp total for distributing surcharges
     line.total = _.sum _.values(line)
     total += line.total
 
+  # Distribute surcharges
   for number, line of bill.lines
     line.sharedSurcharges = bill.surcharges.total * (line.total / total)
     line.total = _.sum _.values(_.omit(line, 'total'))
 
+  # Compute grand total for sanity check
   bill.total = _.sumBy _.values(bill.lines), 'total'
 
+  # Render report page
   res.render 'index', {bill}
 
 app.get '/data', (req, res, next) ->
@@ -102,12 +116,15 @@ app.post '/data', (req, res, next) ->
 
 app.get '/update', (req, res, next) ->
   try
+    # Delete data.json
     fs.unlinkSync './data.json'
   catch
-    # Ignore
+    # Ignore error for file not exists
+  # Start scraping script
   cp.exec 'phantomjs phantom.js', (err) ->
     throw err if err
     console.log "Finish scraping"
+  # Redirect for a loading page
   res.redirect '/'
 
 
